f577f216d9c7840e2c37e0ec9341e0d6665c2a00
Stanley Pena <stanley.pena100@gmail.com>
2025-02-09 18:35:40 -0700

    updates to python file

--- python/sprouts.py
+++ python/sprouts.py
@@ -1,1 +1,10 @@
+##############################################################################
+# Author(s): Madelin A. (@madelinab)
+# Created Date: 2/9/2025
+# Description: Scrapes Sprout's website to get all the items they have available
+# Revision:
+#           2/9/2025 - @madelinab - imported selenium to scrape dynamic web pages
+##############################################################################
+
 import pandas as pd
+from selenium import webdriver
